aea4d9a9c8ceb7abcf0f7877770c99f24faa7a99
Stefan Meyer <stefan-meyer@sinnerschrader.com>
2012-10-17 17:01:55 +0200

    improved concurrency handling

--- src/main/java/org/atemsource/atem/doc/javadoc/JavadocDataStore.java
+++ src/main/java/org/atemsource/atem/doc/javadoc/JavadocDataStore.java
@@ -2,12 +2,10 @@ package org.atemsource.atem.doc.javadoc;
 
 import java.io.File;
 import java.net.URL;
-import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.Map;
-
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
-
 import org.atemsource.atem.api.attribute.Attribute;
 import org.atemsource.atem.api.attribute.relation.SingleAttribute;
 import org.atemsource.atem.api.extension.EntityTypeRepositoryPostProcessor;
@@ -20,116 +18,161 @@ import org.atemsource.atem.impl.meta.MetaAttribute;
 import org.atemsource.atem.impl.meta.MetaDataService;
 import org.atemsource.atem.spi.EntityTypeCreationContext;
 
-public class JavadocDataStore implements MetaDataService,
-		EntityTypeRepositoryPostProcessor, MetaAttributeService {
+
+public class JavadocDataStore implements MetaDataService, EntityTypeRepositoryPostProcessor, MetaAttributeService
+{
 
 	public static final String META_ATTRIBUTE_CODE = "javadocDescription";
 
 	private JAXBContext context;
 
-	private final Map<String, ClassDescription> entityTypeData = new HashMap<String, ClassDescription>();
-
-	public ClassDescription getDescription(Class<?> clazz) {
-		return entityTypeData.get(clazz.getName());
-	}
+	private final Map<String, ClassDescription> entityTypeData = new Hashtable<String, ClassDescription>();
 
 	@Override
-	public <J> SingleAttribute<J> addSingleMetaAttribute(String name,
-			EntityType<?> holderType, EntityType<J> metaDataType) {
+	public <J> SingleAttribute<J> addSingleMetaAttribute(String name, EntityType<?> holderType,
+		EntityType<J> metaDataType)
+	{
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	private ClassDescription convert(ClassDescription classDescription)
+	{
+		return classDescription == null || classDescription.getName() == null ? null : classDescription;
+	}
+
+	public ClassDescription getDescription(Class<?> clazz)
+	{
+		if (entityTypeData.containsKey(clazz.getName()))
+		{
+			return convert(entityTypeData.get(clazz.getName()));
+		}
+		else
+		{
+			load(clazz.getName());
+			ClassDescription classDescription = convert(entityTypeData.get(clazz.getName()));
+			return classDescription;
+		}
+	}
+
 	@Override
-	public Object getMetaData(Object targetEntity, MetaAttribute attribute) {
+	public Object getMetaData(Object targetEntity, MetaAttribute attribute)
+	{
 		// TODO Auto-generated method stub
 
 		// attribute is the attribute in The xml object referencing the object
 		// outside
 		// target entity is the referenced object
 
-		if (targetEntity instanceof Attribute) {
+		if (targetEntity instanceof Attribute)
+		{
 			Attribute targetAttribute = (Attribute) targetEntity;
-			ClassDescription value = entityTypeData.get(targetAttribute
-					.getEntityType().getCode());
-			if (value == null
-					&& !entityTypeData.containsKey(targetAttribute
-							.getEntityType().getCode())) {
+			ClassDescription value = convert(entityTypeData.get(targetAttribute.getEntityType().getCode()));
+			if (value == null && !entityTypeData.containsKey(targetAttribute.getEntityType().getCode()))
+			{
 				load(targetAttribute.getEntityType());
-				value = entityTypeData.get(targetAttribute.getEntityType()
-						.getCode());
-				if (value == null) {
+				value = convert(entityTypeData.get(targetAttribute.getEntityType().getCode()));
+				if (value == null)
+				{
 					return null;
 				}
 				return value.getField(targetAttribute.getCode());
-			} else if (value == null) {
+			}
+			else if (value == null)
+			{
 				return null;
-			} else {
+			}
+			else
+			{
 				return value.getField(targetAttribute.getCode());
 			}
-		} else if (targetEntity instanceof EntityType) {
+		}
+		else if (targetEntity instanceof EntityType)
+		{
 			EntityType targetType = (EntityType) targetEntity;
-			ClassDescription value = entityTypeData.get(targetType.getCode());
-			if (value == null) {
+			ClassDescription value = convert(entityTypeData.get(targetType.getCode()));
+			if (value == null)
+			{
 				load(targetType);
-				return entityTypeData.get(targetType.getCode());
-			} else {
+				return convert(entityTypeData.get(targetType.getCode()));
+			}
+			else
+			{
 				return value;
 			}
-		} else {
+		}
+		else
+		{
 			return null;
 		}
 
 	}
 
 	@Override
-	public void initialize(EntityTypeCreationContext ctx) {
-		try {
-			context = JAXBContext.newInstance(ClassDescription.class,
-					FieldDescription.class);
-			EntityType attributeType = ctx
-					.getEntityTypeReference(Attribute.class);
-			EntityType entityType = ctx
-					.getEntityTypeReference(EntityType.class);
-			EntityType fieldDescription = ctx
-					.getEntityTypeReference(FieldDescription.class);
-			EntityType classDescription = ctx
-					.getEntityTypeReference(ClassDescription.class);
-			MetaAttribute attributeDescription = new MetaAttribute(
-					attributeType, fieldDescription, this, META_ATTRIBUTE_CODE);
-			MetaAttribute entityTypeDescription = new MetaAttribute(entityType,
-					classDescription, this, META_ATTRIBUTE_CODE);
+	public void initialize(EntityTypeCreationContext ctx)
+	{
+		try
+		{
+			context = JAXBContext.newInstance(ClassDescription.class, FieldDescription.class);
+			EntityType attributeType = ctx.getEntityTypeReference(Attribute.class);
+			EntityType entityType = ctx.getEntityTypeReference(EntityType.class);
+			EntityType fieldDescription = ctx.getEntityTypeReference(FieldDescription.class);
+			EntityType classDescription = ctx.getEntityTypeReference(ClassDescription.class);
+			MetaAttribute attributeDescription =
+				new MetaAttribute(attributeType, fieldDescription, this, META_ATTRIBUTE_CODE);
+			MetaAttribute entityTypeDescription =
+				new MetaAttribute(entityType, classDescription, this, META_ATTRIBUTE_CODE);
 			ctx.addMetaAttribute(entityType, entityTypeDescription);
 			ctx.addMetaAttribute(attributeType, attributeDescription);
-		} catch (JAXBException e) {
+		}
+		catch (JAXBException e)
+		{
 			throw new TechnicalException("errors with xml binding", e);
 		}
 
 	}
 
-	private void load(EntityType entityType) {
-		URL resource = getClass().getResource(
-				"/" + entityType.getCode().replace(".", "/") + ".docml");
-		if (resource == null) {
-			entityTypeData.put(entityType.getCode(), null);
+	private void load(Class clazz)
+	{
+		load(clazz.getName());
+	}
+
+	private void load(EntityType<?> entityType)
+	{
+		load(entityType.getCode());
+	}
+
+	private void load(String name)
+	{
+		URL resource = getClass().getResource("/" + name.replace(".", "/") + ".docml");
+		if (resource == null)
+		{
+			entityTypeData.put(name, new ClassDescription());
 			return;
 		}
 		File file = new File(resource.getFile());
-		if (file.exists()) {
+		if (file.exists())
+		{
 			ClassDescription object;
-			try {
-				object = (ClassDescription) context.createUnmarshaller()
-						.unmarshal(file);
-				entityTypeData.put(entityType.getCode(), object);
-			} catch (JAXBException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			try
+			{
+				object = (ClassDescription) context.createUnmarshaller().unmarshal(file);
+				entityTypeData.put(name, object);
 			}
+			catch (JAXBException e)
+			{
+				entityTypeData.put(name, new ClassDescription());
+			}
+		}
+		else
+		{
+			entityTypeData.put(name, new ClassDescription());
 		}
 	}
 
 	@Override
-	public void setMetaData(Object entity, Object value, MetaAttribute attribute) {
+	public void setMetaData(Object entity, Object value, MetaAttribute attribute)
+	{
 		throw new UnsupportedOperationException("cannot be modified");
 	}
 }
--- src/main/java/org/atemsource/atem/doc/javadoc/model/ClassDescription.java
+++ src/main/java/org/atemsource/atem/doc/javadoc/model/ClassDescription.java
@@ -1,10 +1,9 @@
 package org.atemsource.atem.doc.javadoc.model;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
-
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
@@ -12,80 +11,97 @@ import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
+
 @XmlType
 @XmlRootElement(name = "class")
 @XmlAccessorType(XmlAccessType.FIELD)
-public class ClassDescription {
+public class ClassDescription
+{
 	private String description;
-	private String name;
+
+	private Map<String, FieldDescription> fieldMap;
+
 	@XmlElementWrapper(name = "fields")
 	@XmlElement(name = "field")
 	private List<FieldDescription> fields = new ArrayList<FieldDescription>();
 
-	@XmlElementWrapper(name = "methods")
-	@XmlElement(name = "method")
-	private List<MethodDescription> methods = new ArrayList<MethodDescription>();
-
-	public String getDescription() {
-		return description;
-	}
-
-	private Map<String, FieldDescription> fieldMap;
 	private Map<String, MethodDescription> methodMap;
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+	@XmlElementWrapper(name = "methods")
+	@XmlElement(name = "method")
+	private final List<MethodDescription> methods = new ArrayList<MethodDescription>();
 
-	public String getName() {
-		return name;
-	}
+	private String name;
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public List<FieldDescription> getFields() {
-		return fields;
-	}
-
-	public void setFields(List<FieldDescription> fields) {
-		this.fields = fields;
-	}
-
-	public void addField(FieldDescription fieldDescription) {
+	public void addField(FieldDescription fieldDescription)
+	{
 		fields.add(fieldDescription);
 	}
 
-	public void addMethod(MethodDescription methodDescription) {
+	public void addMethod(MethodDescription methodDescription)
+	{
 		methods.add(methodDescription);
 	}
 
-	public FieldDescription getField(String code) {
-		if (fieldMap == null) {
-			synchronized (fieldMap) {
-				if (fieldMap == null) {
-					fieldMap = new HashMap<String, FieldDescription>();
-					for (FieldDescription description : fields) {
-						fieldMap.put(description.getName(), description);
-					}
+	public String getDescription()
+	{
+		return description;
+	}
+
+	public FieldDescription getField(String code)
+	{
+		if (fieldMap == null)
+		{
+			if (fieldMap == null)
+			{
+				fieldMap = new Hashtable<String, FieldDescription>();
+				for (FieldDescription description : fields)
+				{
+					fieldMap.put(description.getName(), description);
 				}
 			}
 		}
 		return fieldMap.get(code);
 	}
 
-	public MethodDescription getMethod(String code) {
-		if (methodMap == null) {
-			synchronized (methodMap) {
-				if (methodMap == null) {
-					methodMap = new HashMap<String, MethodDescription>();
-					for (MethodDescription description : methods) {
-						methodMap.put(description.getName(), description);
-					}
+	public List<FieldDescription> getFields()
+	{
+		return fields;
+	}
+
+	public MethodDescription getMethod(String code)
+	{
+		if (methodMap == null)
+		{
+			methodMap = new Hashtable<String, MethodDescription>();
+			synchronized (methodMap)
+			{
+				for (MethodDescription description : methods)
+				{
+					methodMap.put(description.getName(), description);
 				}
 			}
 		}
 		return methodMap.get(code);
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setDescription(String description)
+	{
+		this.description = description;
+	}
+
+	public void setFields(List<FieldDescription> fields)
+	{
+		this.fields = fields;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
 	}
 }
